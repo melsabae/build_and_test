eeee3a087dc0c1acdb7a943e05073b4d1cdf2b45
Mohammad El-Sabae <mohammad.elsabae@gmail.com>
2017-03-02 20:39:39 -0600

    had a leftover binary that caused some confuzzelation

--- src/main.c
+++ src/main.c
@@ -1,9 +1,19 @@
 #include "main.h"
 #include "second_file.h"
+#include "third_file.h"
 #include <stdio.h>
 
 int main()
 {
+	if(returns_true() != true)
+	{
+		puts("this shouldn't have happened");
+	}
+	else
+	{
+		puts("i expect no less");
+	}
+
 	puts(">>");
 	return 0;
 }
