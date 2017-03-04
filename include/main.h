baeee9eda130c5b5585d712c20b22b87e564e1f4
Mohammad El-Sabae <mohammad.elsabae@gmail.com>
2017-03-03 22:03:23 -0600

    glorious documentation

--- include/main.h
+++ include/main.h
@@ -1,6 +1,33 @@
 #ifndef __MAIN_H__
 #define __MAIN_H__
 
+#include <inttypes.h>
 
+/*! \brief example_struct_t
+ *	This struct represents nothing at all.
+ *
+ * Use to your heart's content.
+ */
+typedef struct
+{
+	uint8_t byte;
+	uint16_t two_byte;
+	uint32_t three_byte;
+} example_struct_t;
+
+int global_int; //!< a global state to be abused
+
+
+/*! \def DOUBLE_RESULT
+ *
+ * Returns x * 2, without modifying x.
+ */
+#define DOUBLE_RESULT(x) (2.0f * x)
+
+/*! \def DOUBLE_STORE
+ *
+ * Returns x * 2, and changes x.
+ */
+#define DOUBLE_STORE(x) (x *= 2.0f)
 
 #endif
--- include/second_file.h
+++ include/second_file.h
@@ -3,8 +3,23 @@
 
 #include <stdbool.h>
 
+/*! \brief A printer function for environmental checks.
+ *	Continuation of the brief description for short line lengths.
+ *
+ * Full description
+ */
 void second_print(void);
+
+/*! \brief A true generator.
+ *
+ * This shall return true for any caller.
+ */
 bool second_true_generator(void);
+
+/*! \brief A false generator.
+ *
+ * This shall return false for any caller.
+ */
 bool second_false_generator(void);
 
 #endif
